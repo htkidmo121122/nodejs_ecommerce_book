02ecad673e858ed75404461db8bdfe9881c1c8e4
Trọng Trần <tranhoangtrong2003@gmail.com>
2024-01-17 10:01:41 +0700

    Upload

---
+++ frontend/src/components/TypeProduct/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const WrapperType = styled.div`
+  padding: 10px 10px;
+  cursor: pointer;
+  &:hover {
+    background-color: #3A4D39;
+    color: #fff;
+    border-radius: 4px;
+  }
+`
